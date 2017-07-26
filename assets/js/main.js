e22acb4752a05f312385b522aa7175ffa70fa62c
DianaQuispe <danaalienmas@gmail.com>
2017-07-25 19:04:14 -0500

    He añadido una tabla para mejorar el codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ var elementosLista = [
   },
   {
     "userId": 7,
-    "id": 123,
+    "id": 12,
     "title": "esse et quis iste est earum aut impedit",
     "completed": false
   },
@@ -66,22 +66,64 @@ var elementosLista = [
     "completed": true
   }
 ];
- 
-function mostrar(title) {
+ function To(userId, id, title) {
+	this.userId = userId;
+	this.id = id;
 	this.title = title;
-}
-
-for(var i = 0; i <elementosLista.length-1 ; i++) {
-//	var str = "";
-//	str+= "<li>" + elementosLista[i].title + "</li>";
-document.getElementById('div').innerHTML += "<li>" + elementosLista[i].title + "</li>";;
+	this.completed = new Boolean(true);
 
 }
+ 
+//.strike()
+
+
+var tabla = document.getElementById("listita");
+for(var re = 0; re <elementosLista.length; re++) {
+	var hilera = document.createElement("tr");
+	var celda1 = document.createElement("td");
+	var celda0 = document.createElement("td");
+	var celda3 = document.createElement("td");
+
+		celda1.innerHTML += "<li>"  + elementosLista[re].title + "</li>"; 
+	/*
+	celda1.innerHTML += "<li>"  + nuevoE + "</li>"; 
+	*/
+	var celda0 = document.createElement("td");
+	hilera.appendChild(celda0);
+	hilera.appendChild(celda1);
+	tabla.appendChild(hilera)
+
+}
+
+
+
+var agregarB = document.getElementById("agregarB");
+agregarB.onclick = function() { 
+
+	var nuevoE = document.getElementById("input").value;
+	var nuevo = {
+    "userId": undefined,
+    "id": undefined,
+    "title": nuevoE,
+    "completed": undefined
+  };
+   	var hilera = document.createElement("tr");
+ 	var tabla = document.getElementById("listita");
+
+    elementosLista.push(nuevo);
+    hilera.appendChild(celda1);
+	tabla.appendChild(hilera)
+
+
+    celda3.innerHTML += "<li>"  + elementosLista[re].title + "</li>"; 
+
+
+}
+
 var agregarB = document.getElementById("agregarB");
 agregarB.onclick = function() {
-	
-	var nuevoE = document.getElementById("input").value;
 
+	var nuevoE = document.getElementById("input").value;
 	var nuevo = {
     "userId": undefined,
     "id": undefined,
@@ -89,10 +131,50 @@ agregarB.onclick = function() {
     "completed": undefined
   };
 
-
-	elementosLista.push(nuevo);
-	document.getElementById('div').innerHTML += "<li>" + nuevoE + "</li>";;
-
-
+  	elementosLista.push(nuevo);
+	document.getElementById('div').innerHTML += "<li>" + nuevoE + "</li>";
 	console.log(elementosLista);
 }
+/*
+function limpiar(){
+	var nuevoE = document.getElementById("input").value;
+	nuevoE.value = "";
+
+}
+*/
+
+/*
+var celdaa1 = document.createElement("tr");
+var agregarB = document.getElementById("agregarB");
+agregarB.onclick = function() {
+		var hilera1 = document.createElement("tr");
+
+	hilera1.appendChild(celdaa1);
+		tabla.appendChild(hilera)
+
+celdaa1.innerHTML += "<li>"  + nuevoE + "</li>";
+var nuevoE = document.getElementById("input").value;
+var nuevo = {
+    "userId": undefined,
+    "id": undefined,
+    "title": nuevoE,
+    "completed": undefined
+  };
+
+elementosLista.push(nuevo);
+document.getElementById('div').innerHTML += "<li>" + nuevoE + "</li>";
+	console.log(elementosLista);
+}
+
+/*
+function mostrar(title) {
+	this.title = title;
+
+}
+
+
+for(var i = 0; i <elementosLista.length-1 ; i++) {
+	document.getElementById('div').innerHTML += "<li>"  + elementosLista[i].title + "</li>";
+}
+
+	*/
